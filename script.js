326fedb2f6d734fb88a1e00f5c054a44fee83aaa
kent <kent.26pg@gmail.com>
2026-06-15 13:37:45 +0200

    add core logic for saving and retrieving books via localStorage

---
+++ script.js
@@ -0,0 +1,37 @@
+const key = "booklog";
+
+function getBooks() {
+  return JSON.parse(localStorage.getItem(key)) || [];
+}
+
+function saveBooks(books) {
+  localStorage.setItem(key, JSON.stringify(books));
+}
+
+function addBook() {
+  const tittel = document.getElementById(`titleInput`).value.trim();
+  if (!tittel) {
+    alert("Tittel er påkrevd");
+    return;
+  }
+
+const books = getBooks();
+
+books.push({
+  id: Date.now(),
+  title: document.getElementById(`titleInput`).value.trim(),
+  author: document.getElementById(`authorInput`).value.trim(),
+  genre: document.getElementById(`genreInput`).value,
+  favoritt: false,
+});
+
+saveBooks(books);
+showBooks();
+}
+function showBooks() {
+  const books = getBooks();
+  console.log("Current books in storage:", books);
+}
+document.getElementById("addBtn").addEventListener("click", addBook);
+
+console.log(localStorage)
